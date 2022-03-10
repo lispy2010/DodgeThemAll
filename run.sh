1d825444cec5689bd4b16754d7840ccfe9ba5db7
lispy2010 <79205601+lispy2010@users.noreply.github.com>
2022-03-10 18:50:36 +0300

    Delete run.sh (it was in wrong folder)

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# check if script is run in scripts folder
-if [ ! -d "scripts" ]; then
-    echo "Please run this script in the scripts folder"
-    exit 1
-fi
-bash compile.sh
-python3 minify.py
-npx electron ../main.js
